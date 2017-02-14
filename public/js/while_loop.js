9971948df6566ce0302418b706a9b66e83b7b3f0
Thomas Hayes <thomashayes7889@gmail.com>
2017-02-14 15:09:25 -0600

    Adding lectures

--- public/js/while_loop.js
+++ public/js/while_loop.js
@@ -10,17 +10,26 @@
 
 //question 1
  // This is how you get a random number between 50 and 100
-var allCones = Math.floor(Math.random() * 50) + 50;
+var allCones /*coneInventory */ = Math.floor(Math.random() * 50) + 50;
  // This is how you get a random number between 1 and 5
 var cones = Math.floor(Math.random() * 5) + 1;
-var i = 5;
-var calculatedCones = allCones-cones;
+
+do {
+	var conesToSell = Math.floor(Math.random() *5) +1;
+	// if we have that many left, subtract it frmo our inventory
+	if (conesToSell <= allCones/* we have enough left */) {
+		//subtract from inventory
+		console.log("sold " + conesToSell + " cones...")
+		allCones = (allCones - conesToSell);
+	} else {
+		console.log("I don't have enough!")
+	}
+} while (allCones > 0/* we have cones left to sell*/)
+
+	console.log("sold all the cones!");
 
 
- do {
- 	console.log(cones + " sold"); 
- 	i++;
 
- } while (i <= 5);
-console.log(allCones);
-console.log(calculatedCones);
+//If you are finished...
+//write some code such that a random *even* number will be console.logged
+//everytime the page is refreshed
