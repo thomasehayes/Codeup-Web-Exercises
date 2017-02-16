5260ef6e7c8ede056a0ce747e780ecc0474dbc96
Thomas Hayes <thomashayes7889@gmail.com>
2017-02-16 09:35:07 -0600

    Adding Array exercises to Git

---
+++ public/js/iterating.js
@@ -0,0 +1,13 @@
+(function(){
+    "use strict";
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+    var names = ["Thomas", "Alissa", "Katie", "Quang"];
+
+
+    // TODO: Create a log statement that will log the number of elements in the names array.
+    console.log(names.length);
+
+    // TODO: Create log statements that will print each of the names array elements individually.
+    console.log(names);
+})();
