21f0183dabead7a001f665f971e6a41a7207b388
Thomas Hayes <thomashayes7889@gmail.com>
2017-02-16 09:56:40 -0600

    Adding updated lecture notes from 2-15 and for/for-each loop updates in iterating.js

--- public/js/iterating.js
+++ public/js/iterating.js
@@ -1,16 +1,33 @@
 (function(){
-    "use strict";
+"use strict";
 
-    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-    var names = ["Thomas", "Alissa", "Katie", "Quang"];
+//     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+     var names = ["Thomas", "Alissa", "Katie", "Quang"];
 
 
-    // TODO: Create a log statement that will log the number of elements in the names array.
-    console.log(names.length);
+//     // TODO: Create a log statement that will log the number of elements in the names array.
+//     console.log(names.length);
 
-    // TODO: Create log statements that will print each of the names array elements individually.
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-    console.log(names[3]);
-})();
+//     // TODO: Create log statements that will print each of the names array elements individually.
+//     console.log(names[0]);
+//     console.log(names[1]);
+//     console.log(names[2]);
+//     console.log(names[3]);
+
+for (var i = 0; i < names.length; i++) {
+	console.log(names[i]);
+}
+
+
+
+ names.forEach (function(element, index, array) {
+ 	console.log(element);
+ });
+
+ 
+ })();
+
+
+
+ 
+
