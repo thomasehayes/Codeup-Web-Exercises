c9b8b8014d5c7ec40207122650a8398164b29967
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-28 14:19:31 -0500

    Added final solution to sessions exercise

---
+++ public/login.php
@@ -0,0 +1,93 @@
+<?php 
+
+session_start();
+
+// get the current session ID
+$sessionId = session_id();
+
+$message = "";
+// check if the form was submitted
+if(!empty($_POST)) {
+	$username = isset($_POST['username']) ? $_POST['username'] : "";
+	$password = isset($_POST['password']) ? $_POST['password'] : "";
+	if($username == "guest" && $password == "password") {
+		 $_SESSION['logged_in_user'] = $_POST['username'];
+		if(isset($_SESSION['logged_in_user'])){
+			header("Location: /authorized.php");
+		exit;
+	}
+	} else {
+		$message = "Please retry your username or password";
+		
+	}
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+	
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<meta name="description" content="">
+	<meta name="Keywords" content="">
+    <meta name="author" content="">
+	<title>Login</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Custom CSS -->
+
+<style>
+	body {
+		background-image:url("img/background.jpg");
+	}
+
+	h1, label {
+		color: white;
+	}
+
+	main {
+		top: 100px;
+		left: 100px;
+	}
+</style>
+
+
+</head>
+<body>
+	<main class="container col-xs-6 col-md-offset-4" >
+		<h1>Login to gain access!</h1>
+		<p><?= $message ?></p>
+		<?php if(!empty($image)): ?>
+			<img src="<?= $image ?>">
+		<?php endif; ?>
+
+		<form action="/login.php" method="POST" class="col-xs-4">
+			<div class="form-group">
+		    	<label for="username">Username</label>
+		    	<input type="text" name="username" class="form-control" id="username" placeholder="Username">
+		  	</div>
+		  	<div class="form-group">
+    			<label for="password">Password</label>
+    			<input type="password" name="password" class="form-control" id="password" placeholder="Password">
+  			</div>
+  			<button type="submit" class="btn btn-default">Login</button>
+		</form>
+	</main>
+
+	<!-- minified jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<!-- Your custom JS goes here -->
+	<script type="text/javascript"></script>
+</body>
+</html>
---
+++ public/logout.php
@@ -0,0 +1,73 @@
+<?php 
+session_start();
+
+function clearSession()
+{
+    // clear $_SESSION array
+    session_unset();
+
+    // delete session data on the server
+    session_destroy();
+
+    // ensure client is sent a new session cookie
+    session_regenerate_id();
+
+    // start a new session - session_destroy() ended our previous session so
+    // if we want to store any new data in $_SESSION we must start a new one
+    session_start();
+}
+clearSession();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="">
+    <meta name="Keywords" content="">
+    <meta name="author" content="">
+    <title>Logout</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Custom CSS -->
+
+<style>
+
+    body {
+        background-image:url("img/background.jpg");
+    }
+    h1 {
+        color: white;
+    }
+    .container {
+        text-align: center;
+    }
+</style>
+
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>You are Logged Out!</h1>
+        <a href="/login.php">Click here to return to Login Page!</a>
+	</div>
+
+<!-- minified jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <!-- Your custom JS goes here -->
+    <script type="text/javascript"></script>
+
+</body>
+</html>
