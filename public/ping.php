438a65467cfcdb0d174f299e403e0890acfed006
Thomas Hayes <thomashayes7889@gmail.com>
2017-03-30 15:08:22 -0500

    refactored ping pong files with static input has and get.

--- public/ping.php
+++ public/ping.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "functions.php";
+require_once "Input.php";
 
 // on page load, set counter to 0
 // make a button or link that increments the counter by 1
@@ -10,8 +11,8 @@ function pageController() {
 	$data = [];
 	
 	//if a value exists, get that value and assign to variable, else assign default value
-	if(inputHas('count')) {
-		$data['count'] = inputGet('count');
+	if(Input::has('count')) {
+		$data['count'] = Input::get('count');
 	} else {
 		$data['count'] = 0;
 	}
@@ -33,8 +34,8 @@ extract(pageController());
 <body>
 	<main class="container">
 		<h1>Ping!</h1>
-		<h1>Counter: <?= escape($count) ?></h1>
-		<a class="btn btn-primary" href="pong.php?count=<?= $count +1 ?>">Hit</a>
+		<h1>Counter: <?= ($count) ?></h1>
+		<a class="btn btn-primary" href="pong.php?count=<?= ($count +1) ?>">Hit</a>
 		<a class="btn btn-primary" href="pong.php?count=0">Miss</a>
 
 
--- public/pong.php
+++ public/pong.php
@@ -1,5 +1,6 @@
 <?php
 require_once "functions.php";
+require_once "Input.php";
 
 // on page load, set counter to 0
 // make a button or link that increments the counter by 1
@@ -9,8 +10,8 @@ function pageController() {
 	$data = [];
 	
 	//if a value exists, get that value and assign to variable, else assign default value
-	if (inputHas('count')) {
-		$data['count'] = inputGet('count');
+	if (Input::has('count')) {
+		$data['count'] = Input::get('count');
 	} else {
 		$data['count'] = 0;
 	}
@@ -32,8 +33,8 @@ extract(pageController());
 <body>
 	<main class="container">
 		<h1>Pong!</h1>
-		<h1>Counter: <?= escape($count) ?></h1>
-		<a class="btn btn-primary" href="ping.php?count=<?= $count +1 ?>">Hit</a>
+		<h1>Counter: <?= ($count) ?></h1>
+		<a class="btn btn-primary" href="ping.php?count=<?= ($count +1) ?>">Hit</a>
 		<a class="btn btn-primary" href="ping.php?count=0">Miss</a>
 
 
