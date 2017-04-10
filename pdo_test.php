74da5c78f4d7d07e3b0a85a4cd56c8ab4b848658
Thomas Hayes <thomashayes7889@gmail.com>
2017-04-10 10:14:24 -0500

    lecture note from mysql

--- pdo_test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-define('DB_HOST'. 'localhost'); // A constant definition
-define('DB_NAME', 'employees'); // A constant definition
-define('DB_USER', 'vagrant'); // A constant definition
-define('DB_PASSWORD', 'vagrant'); // A constant definition
-
- ?>
---
+++ public/session_debug/functions.php
@@ -0,0 +1,36 @@
+
+<?php
+function redirect($location)
+{
+    header("Location: $location");
+    exit;
+}
+function isUserAuthenticated()
+{
+    return isset($_SESSION['logged_in_user']);
+}
+function user()
+{
+    return isset(isUserAuthenticated()) ? ($_SESSION['logged_in_user']) : null;
+}
+function authenticate($username, $password)
+{
+    if ($username == 'guest' && $password == 'password') {
+        $_SESSION['logged_in_user'] = $username;
+        return true;
+    }
+    return false;
+}
+function isPost()
+{
+    return $_SERVER['REQUEST_METHOD'] === 'POST';
+}
+function input($key, $default = '')
+{
+    return isset($_REQUEST[$key]) ? $_REQUEST[$key] : $default;
+}
+function clearSession()
+{
+    session_unset();
+    session_regenerate_id(true);
+}
